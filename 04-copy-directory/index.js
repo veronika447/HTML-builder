dfbb64077d92e7ff83e9876930d0b0eab3b2bfd1
veronika447 <starenkaya.veronika@gmail.com>
2023-05-09 00:01:58 +0300

    feat: add function copyDir

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+const copyDir = () => {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, () => { });
+  fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+    if (err)
+      console.log(err);
+    else {
+      files.forEach(file => {
+        fs.copyFile(path.join(__dirname, 'files', file), path.join(__dirname, 'files-copy', file), () => { });
+      });
+    }
+  });
+};
+
+copyDir();
 
